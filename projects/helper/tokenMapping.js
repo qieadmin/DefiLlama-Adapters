d104ef0000a42e35920d3cebe1cce0f2c244e6ff
qieadmin <info@qiewallet.me>
2025-08-25 11:47:55 +0530

    Added new blockchain (QIEV3 chain id - 1990)

--- projects/helper/tokenMapping.js
+++ projects/helper/tokenMapping.js
@@ -39,6 +39,9 @@ const ibcMappings = {
 }
 
 const fixBalancesTokens = {
+   qiev3: {
+    "0x0000000000000000000000000000000000000000": { coingeckoId: "qi-blockchain", decimals: 18 }
+  },
   ozone: {
     // '0x83048f0bf34feed8ced419455a4320a735a92e9d': { coingeckoId: "ozonechain", decimals: 18 }, // was mapped to wrong chain
   },
---
+++ projects/qieswap/index.js
@@ -0,0 +1,3 @@
+const { uniTvlExport } = require('../helper/unknownTokens')
+
+module.exports = uniTvlExport('QIEV3', '0x8E23128a5511223bE6c0d64106e2D4508C08398C') //blockchain, factory address
